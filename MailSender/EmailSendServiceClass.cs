fc337dc72d9c3f1f527a49fffe8877c2b02cc7d6
Виктор Орлов <3841832@gmail.com>
2018-12-25 20:14:26 +0300

    1

---
+++ MailSender/EmailSendServiceClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+namespace MailSender
+{
+    class EmailSendServiceClass
+    {
+        MailMessage mm = new MailMessage(@"berlin.22014@yandex.ru", "3841832@gmail.com","тест","етст");
+        SmtpClient sc = new SmtpClient(WpfTestMailSender.smtp_adress, WpfTestMailSender.smtp_port);        
+    }
+}
---
+++ MailSender/WpfTestMailSender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender
+{
+    public static class WpfTestMailSender
+    {
+        public static string smtp_adress = "smtp.yandex.ru";
+        public static int smtp_port = 25;
+    }
+}
