9921d7cd4cca6879a6e736388570db9681b098c6
Wintego <3841832@gmail.com>
2019-01-24 17:58:08 +0300

    5 задание

---
+++ hw8-5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw8_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Table
+    {
+        public int id { get; set; }
+        public int group_id { get; set; }
+        public int descr { get; set; }
+    }
+
+    class DB : DbContext
+    {
+        public DB() : this("name=hw8") { }
+        public DB(string connectionString) : base(connectionString) { }
+        static DB() => System.Data.Entity.Database.SetInitializer(new MigrateDatabaseToLatestVersion<DB, hw8_5.Migrations.Configuration>(true));
+        public DbSet<Table> Table;
+    }
+}
